cb3d06af79866fcba80aa0fddde3531ff33410c1
V1cEvyto <110555196+V1cEvyto@users.noreply.github.com>
2022-10-29 22:28:48 -0300

    Add files via upload

---
+++ pag.js
@@ -0,0 +1,13 @@
+
+function selecoes(){
+    const element = document.getElementById('primeiraSelect');
+        const valor = element.options[element.selectedIndex].text;
+    return(
+        
+    
+        console.log(valor)
+    )
+}
+
+
+export default selecoes()   
